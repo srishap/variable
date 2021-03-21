51527b3a90e4d31388b2a2f6f70d08d5b263e5c4
srisha p <srishapyati4@gmail.com>
2021-03-21 06:07:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,21 @@
+#assign variables
+a=10
+b=10.12
+c='srisha'
+#print variables
+print(a)
+print(b)
+print(c)
+
+#multiple assign variables
+#1.single  value to the diff variables
+x=y=z=1
+print(x)
+print(y)
+print(z)
+#2.separately we can assign the values to each variables
+
+i,j,k=2,3.3,'pyati'
+print(k)
+print(i)
+print(j)
